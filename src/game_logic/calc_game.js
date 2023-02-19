cd88cbe6d7683bb8155db60a0c661f88fa2e5bc4
Alexander Shmelev <120136549+felarn@users.noreply.github.com>
2023-02-19 12:37:04 +0300

    brain-calc game is added

---
+++ src/game_logic/calc_game.js
@@ -0,0 +1,30 @@
+import rng from "../utils/rng.js";
+
+export default (extra=null) => {
+    if (extra==='rules') return 'What is the result of the expression?'
+
+    const N1 = rng(10);
+    const N2 = rng(10);
+    let quest = '';
+    let answer ='';
+
+    switch (rng(1,3)){
+        case 1:
+            quest = `${N1} + ${N2}`;
+            answer = String(N1 + N2);
+            break
+
+        case 2:
+            quest = `${N1} - ${N2}`;
+            answer = String(N1 - N2);
+            break
+
+        case 3:
+            quest = `${N1} * ${N2}`;
+            answer = String(N1 * N2);
+            break
+    }
+
+
+    return [quest,answer]
+}
--- src/utils/game_selector.js
+++ src/utils/game_selector.js
@@ -1,4 +1,5 @@
 import even from '../game_logic/even_game.js';
+import calc from '../game_logic/calc_game.js';
 
 export default (gameName, action) => {
   return eval(gameName)(action);
