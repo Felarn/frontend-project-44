0a41edd0f2817c6fe5b6a5cdf8dba1686b980970
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-02-19 13:22:23 +0300

    fixed style

--- src/game_logic/calc_game.js
+++ src/game_logic/calc_game.js
@@ -1,30 +1,32 @@
-import rng from "../utils/rng.js";
+import rng from '../utils/rng.js';
 
-export default (extra=null) => {
-    if (extra==='rules') return 'What is the result of the expression?'
+export default (extra = null) => {
+  if (extra === 'rules') return 'What is the result of the expression?';
 
-    const N1 = rng(10);
-    const N2 = rng(10);
-    let quest = '';
-    let answer ='';
+  const N1 = rng(10);
+  const N2 = rng(10);
+  let quest = '';
+  let answer = '';
 
-    switch (rng(1,3)){
-        case 1:
-            quest = `${N1} + ${N2}`;
-            answer = String(N1 + N2);
-            break
+  switch (rng(1, 3)) {
+    case 1:
+      quest = `${N1} + ${N2}`;
+      answer = String(N1 + N2);
+      break;
 
-        case 2:
-            quest = `${N1} - ${N2}`;
-            answer = String(N1 - N2);
-            break
+    case 2:
+      quest = `${N1} - ${N2}`;
+      answer = String(N1 - N2);
+      break;
 
-        case 3:
-            quest = `${N1} * ${N2}`;
-            answer = String(N1 * N2);
-            break
-    }
+    case 3:
+      quest = `${N1} * ${N2}`;
+      answer = String(N1 * N2);
+      break;
 
+    default:
+      console.error('wrong index for math operation');
+  }
 
-    return [quest,answer]
-}
+  return [quest, answer];
+};
--- src/utils/game_selector.js
+++ src/utils/game_selector.js
@@ -1,6 +1,4 @@
 import even from '../game_logic/even_game.js';
 import calc from '../game_logic/calc_game.js';
 
-export default (gameName, action) => {
-  return eval(gameName)(action);
-};
+export default (gameName, action) => eval(gameName)(action);
