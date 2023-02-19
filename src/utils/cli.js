3bdade10b806a01c8321d3da0707e49fe70c0940
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-02-19 21:36:28 +0300

    greetings fixed

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello ${playerName}`);
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${playerName}!`);
   return playerName;
 };
