b3ddab8e3678b8aef31bc6f5c41974722de07e23
Felarn <120136549+Felarn@users.noreply.github.com>
2023-03-03 10:31:41 +0300

    Update rng.js

--- src/utils/rng.js
+++ src/utils/rng.js
@@ -1,11 +1,6 @@
-export default (arg1 = 10, arg2 = null) => {
-  let minValue = arg1;
-  let maxValue = arg2;
-
-  if (arg2 === null) {
-    minValue = 0;
-    maxValue = arg1;
-  }
+export default (arg1, arg2 = 0) => {
+  minValue = Math.min(arg1,arg2);
+  maxValue = Math.max(arg1,arg2);
 
   return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 };
